317ad028b43e4ce6c51a23e6feaa87f5397189c1
tk229 <hakuneru2222@gmail.com>
2022-04-15 13:46:01 +1200

    End  15/04/2022

--- COMPX323Milestone2/Oracle.cs
+++ COMPX323Milestone2/Oracle.cs
@@ -4,13 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Oracle.ManagedDataAccess.Client;
+using System.Data;
+
 
 namespace COMPX323Milestone2
+
 {
     class Oracle
     {
-        string oradb = "Data Source=ORCL;User Id=hr;Password=hr;";
-        OracleConnection conn = new OracleConnection(oradb);  // C#
-        conn.Open();
+        
+        public static string oradb = "Data Source=oracle.cms.waikato.ac.nz:1521/teaching;User Id=tk229;Password=;";
+        public static  OracleConnection conn = new OracleConnection(oradb);  // C#
+        
+        public static void selectQuery(string query)
+        {
+            try
+            {
+                conn.Open();
+                OracleCommand cmd = new OracleCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = query;
+                cmd.CommandType = CommandType.Text;
+                OracleDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+                //label1.Text = dr.GetString(0);
+                conn.Dispose();
+            }
+            catch
+            {
+                return;
+            }
+        }
     }
 }
