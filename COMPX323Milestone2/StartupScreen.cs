f9841042d885c1ffe7f4f6d8be90d3c646735d65
kh216 <kevinhan938@gmail.com>
2022-04-15 12:08:13 +1200

    Commit 1: Add team cs, Change Form1 to StartupScreen

--- COMPX323Milestone2/StartupScreen.cs
+++ COMPX323Milestone2/StartupScreen.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace COMPX323Milestone2
 {
-    public partial class Form1 : Form
+    public partial class StartupScreen : Form
     {
-        public Form1()
+        public StartupScreen()
         {
             InitializeComponent();
         }
