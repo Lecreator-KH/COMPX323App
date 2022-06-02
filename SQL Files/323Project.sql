3d6510fe03cd552251d9cd9f859fc3b3eac730ec
Kevin <kh216@students.waikato.ac.nz>
2022-06-02 18:39:19 +1200

    Added indexes to sql. B-tree and Hash Cluster

--- SQL Files/323Project.sql
+++ SQL Files/323Project.sql
@@ -27,16 +27,16 @@ Username varchar(30),
 Organization varchar(50) not null,
 PhoneNumber varchar(20),
 
-CONSTRAINT PKPlayer PRIMARY KEY (Username),
-CONSTRAINT FKPlayer FOREIGN KEY (Username) REFERENCES Person ON DELETE CASCADE
+CONSTRAINT PKOrg PRIMARY KEY (Username),
+CONSTRAINT FKOrg FOREIGN KEY (Username) REFERENCES Person ON DELETE CASCADE
 );
 
 create table Player(
 Username varchar(30),
 TeamID varchar(30),
 
-CONSTRAINT PKOrg PRIMARY KEY (Username),
-CONSTRAINT FKOrg FOREIGN KEY (Username) REFERENCES Person ON DELETE CASCADE,
+CONSTRAINT PKPlayer PRIMARY KEY (Username),
+CONSTRAINT FKPlayer FOREIGN KEY (Username) REFERENCES Person ON DELETE CASCADE,
 CONSTRAINT FKTeamID FOREIGN KEY (TeamID) REFERENCES Team 
 );
 create table Setting(
@@ -144,47 +144,46 @@ Select * From SportEvent;
 Select * From ViewingExperience;
 Select * From RunIn;
 Select * From Attends;
-<<<<<<< HEAD
-Select eventdate, TO_CHAR(startTime , 'HH:MI'), location, person From Watches;
+Select eventdate, TO_CHAR(startTime , 'HH24:MI'), location, person From Watches;
 
 -- Hash
 -- B-Tree Index
-Create Index bTreeMaxplayer on Sport(Maxplayer);
-Create Index bTreeMinplayer on Sport(Minplayer);
+Create Index btree on Person(firstname);
+Drop index btree;
 
-Drop index bTreeMaxplayer;
-Drop index bTreeMinplayer;
+explain plan for
+Select * from Person where firstname like 'nu%q%';
 
-Select * From Sport where minplayer > 1 and minplayer < 3;
-Select * From Sport where maxplayer > 7 and maxplayer < 10;
-
-
+SELECT 
+    PLAN_TABLE_OUTPUT 
+FROM 
+    TABLE(DBMS_XPLAN.DISPLAY());
 -- Hash cluster
-create table Team(
-TeamID NUMBER(*,0),
-Name varchar(30) not null,
-TeamEmail varchar(50) not null,
-PhoneNumber varchar(20),
-Website varchar(50) not null,
-Sport varchar(30),
+DROP table SportEventTest;
+Drop cluster hashcluster;
 
-CONSTRAINT FKSport foreign key(Sport) references Sport(Name)
-) cluster hashCluster(TeamID);
+create table SportEventTest(
+EventDate Date,
+StartTime Date,
+Location varchar(30),
+EndTime Date,
+Organizer varchar(30),
+MainSponsor varchar(50) not null,
+Description varchar(100),
 
-DROP table Team;
+CONSTRAINT PKSportEventTest PRIMARY KEY (EventDate, StartTime, Location),
+CONSTRAINT FKLocationTest FOREIGN KEY(Location) REFERENCES Venue(Location),
+CONSTRAINT FKOrganizerTest FOREIGN KEY(Organizer) REFERENCES Organizer(Username)
+)cluster hashCluster(Organizer);
+
 Create cluster hashCluster (
-    TeamID number(*,0))
-    HASHKeys 100 Hash is TeamID
-    size 256;
-    
-Drop cluster hashCluster;
+    Organizer varchar(30))
+    size 256 HASHKeys 100;
 
-Select * From Team Where (TeamID = 99);
-=======
-Select TO_CHAR(startTime, 'HH:MI') From Watches;
+explain plan for
+Select * from SportEvent where organizer = 'umtbverr90633';
 
->>>>>>> 9e6455849451fa08cec13910a11ac780b59f5d0b
-
-
-
-
+SELECT 
+    PLAN_TABLE_OUTPUT 
+FROM 
+    TABLE(DBMS_XPLAN.DISPLAY());
