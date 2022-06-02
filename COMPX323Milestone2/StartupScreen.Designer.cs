76867cca6f6f1b26294dda6c8d42674d933252d8
ba81 <ba81@waikato.ac.nz>
2022-06-02 16:28:54 +1200

    Test commit

--- COMPX323Milestone2/StartupScreen.Designer.cs
+++ COMPX323Milestone2/StartupScreen.Designer.cs
@@ -32,14 +32,14 @@
             this.buttonLoginViewer = new System.Windows.Forms.Button();
             this.buttonLoginOrganiser = new System.Windows.Forms.Button();
             this.buttonLoginTeam = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // buttonRegister
             // 
-            this.buttonRegister.Location = new System.Drawing.Point(541, 130);
-            this.buttonRegister.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonRegister.Location = new System.Drawing.Point(196, 97);
             this.buttonRegister.Name = "buttonRegister";
-            this.buttonRegister.Size = new System.Drawing.Size(200, 58);
+            this.buttonRegister.Size = new System.Drawing.Size(150, 50);
             this.buttonRegister.TabIndex = 0;
             this.buttonRegister.Text = "Register";
             this.buttonRegister.UseVisualStyleBackColor = true;
@@ -47,10 +47,9 @@
             // 
             // buttonLoginViewer
             // 
-            this.buttonLoginViewer.Location = new System.Drawing.Point(541, 284);
-            this.buttonLoginViewer.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonLoginViewer.Location = new System.Drawing.Point(196, 230);
             this.buttonLoginViewer.Name = "buttonLoginViewer";
-            this.buttonLoginViewer.Size = new System.Drawing.Size(200, 58);
+            this.buttonLoginViewer.Size = new System.Drawing.Size(150, 50);
             this.buttonLoginViewer.TabIndex = 1;
             this.buttonLoginViewer.Text = "login as a viewer";
             this.buttonLoginViewer.UseVisualStyleBackColor = true;
@@ -58,10 +57,9 @@
             // 
             // buttonLoginOrganiser
             // 
-            this.buttonLoginOrganiser.Location = new System.Drawing.Point(541, 367);
-            this.buttonLoginOrganiser.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonLoginOrganiser.Location = new System.Drawing.Point(196, 302);
             this.buttonLoginOrganiser.Name = "buttonLoginOrganiser";
-            this.buttonLoginOrganiser.Size = new System.Drawing.Size(200, 58);
+            this.buttonLoginOrganiser.Size = new System.Drawing.Size(150, 50);
             this.buttonLoginOrganiser.TabIndex = 2;
             this.buttonLoginOrganiser.Text = "login as a organiser";
             this.buttonLoginOrganiser.UseVisualStyleBackColor = true;
@@ -69,28 +67,37 @@
             // 
             // buttonLoginTeam
             // 
-            this.buttonLoginTeam.Location = new System.Drawing.Point(541, 205);
-            this.buttonLoginTeam.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.buttonLoginTeam.Location = new System.Drawing.Point(196, 162);
             this.buttonLoginTeam.Name = "buttonLoginTeam";
-            this.buttonLoginTeam.Size = new System.Drawing.Size(200, 58);
+            this.buttonLoginTeam.Size = new System.Drawing.Size(150, 50);
             this.buttonLoginTeam.TabIndex = 3;
             this.buttonLoginTeam.Text = "login as a team";
             this.buttonLoginTeam.UseVisualStyleBackColor = true;
             this.buttonLoginTeam.Click += new System.EventHandler(this.buttonTeam_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SportViewer 22";
+            // 
             // StartupScreen
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 519);
+            this.ClientSize = new System.Drawing.Size(396, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonLoginTeam);
             this.Controls.Add(this.buttonLoginOrganiser);
             this.Controls.Add(this.buttonLoginViewer);
             this.Controls.Add(this.buttonRegister);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.Name = "StartupScreen";
             this.Text = "StartupScreen";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -100,6 +107,7 @@
         private System.Windows.Forms.Button buttonLoginViewer;
         private System.Windows.Forms.Button buttonLoginOrganiser;
         private System.Windows.Forms.Button buttonLoginTeam;
+        private System.Windows.Forms.Label label1;
     }
 }
 
